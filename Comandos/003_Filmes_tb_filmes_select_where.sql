7cf03a947dca1a736f4b8c7f5644e3372affcfa1
Lucas-RM <lucas.marcondes36@gmail.com>
2024-09-18 16:51:23 -0300

    Adicionando o arquivo com a solução dos desafios 3, 4, 5 e 6

---
+++ Comandos/003_Filmes_tb_filmes_select_where.sql
@@ -0,0 +1,19 @@
+SELECT * FROM Filmes;
+
+/* 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração */
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+/* 4 - Buscar os filmes lançados em 1997 */
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Ano = 1997;
+
+/* 5 - Buscar os filmes lançados APÓS o ano 2000 */
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Ano > 2000;
+
+/* 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, 
+	ordenando pela duracao em ordem crescente */
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC;
